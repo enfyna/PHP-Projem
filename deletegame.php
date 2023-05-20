f0db4ef203ce70e77e7b914103996e59ee31199f
Ramazan Aslan <91965312+enfyna@users.noreply.github.com>
2023-05-20 20:05:54 +0300

    Update deletegame.php

--- deletegame.php
+++ deletegame.php
@@ -1,31 +1,30 @@
-<?php include_once "site/head.php"; ?>
-<?php include_once "site/header.php"; ?>
-
+<?php include_once "site/head.php";include_once "site/header.php"; ?>
 <main>
-    <div>
-        <h1>Oyunu Sil</h1>
-    </div>
-    <?php 
+<div>
+    <h1>Oyunu Sil</h1>
+</div>
+<?php
 require_once "library/config.php";
 require_once "library/functions.php";
 require_once "library/kick_anon.php";
-
 // Kullanıcı oyunu silmişse onu ayarla
-if(isset($_POST["delete"]) && $_POST["delete"] == "true"){
-    $game_id = $_POST["game_id"];
-
-    mysqli_query($db,
-        "DELETE games,ratings FROM games LEFT JOIN ratings ON games.game_id = ratings.game_id WHERE games.game_id = '$game_id' ");
+if(isset($_POST["delete"])){
+    if($_POST["delete"] == "true"){
+        $game_id = $_POST["game_id"];
+        mysqli_query($db,
+            "DELETE games,ratings FROM games LEFT JOIN ratings ON games.game_id = ratings.game_id WHERE games.game_id = '$game_id' ");
+    };
     redirectTo("index.php",0);
     exit;
-}
-else if(!isset($_POST["game_id"]))exit;//redirectTo("index.php",0);
-
+}else if(!isset($_POST["game_id"])){
+    redirectTo("index.php",0);
+    exit;
+};
 $game_id = $_POST["game_id"];
-$q = mysqli_query($db, // Oyun bilgilerini ve oyunu ekleyen kullanıcının bilgilerini al 
+$q = mysqli_query($db, // Oyun bilgilerini al
     "SELECT games.* FROM games WHERE games.game_id = '$game_id'");
 $row_num = mysqli_num_rows($q);
-if($row_num == 0) exit;//redirectTo("index.php",0); // Oyun IDsi bulunamazsa ana menuye geri dön
+if($row_num == 0) redirectTo("index.php",0); // Oyun IDsi bulunamazsa ana menuye geri dön
 
 $game_data = mysqli_fetch_assoc($q); // Oyun bilgilerini al
 
@@ -35,24 +34,23 @@ $game_release_date = $game_data["game_release_date"];
 $game_genre = $game_data["game_genre"];
 $game_description = $game_data["game_description"];
 $game_image = $game_data["game_image"];
-
 ?>
-    <div>
-        <form class="form" id="gameForm" action="deletegame.php" method="post">
-            <h1><?php echo $game_name ?></h1>
-            <br><br>
-            <h2>Bu oyunu silmek istediğine emin misin?</h2>
-            <br><br><br><br>
-            <div class="fw center">
-                <button class="btn green p1 wt" onclick="window.location.href='index.php';">Vazgeç</button>
-                <button class="btn red p1 wt" type="submit" name="delete" value="true">Sil</button>
-            </div>
-            <br><br><br><br>
-            <h3>Bu oyuna <?php echo $_POST["comment_num"];?> yorum yapılmış. Hepsi kaybolacak .</h3>
-            <br><br><br><br>
-            <h3>Sonsuza kadar.😱</h3>
-            <input type="hidden" name="game_id" value="<?php echo $game_id ?>">
-        </form>
-    </div>
+<div>
+    <form class="form" id="gameForm" action="deletegame.php" method="post">
+        <h1><?php echo $game_name ?></h1>
+        <br><br>
+        <h2>Bu oyunu silmek istediğine emin misin?</h2>
+        <br><br>
+        <div class="fw center">
+            <button class="btn green p1 wt" type="submit" name="delete" value="false">Vazgeç</button>
+            <button class="btn red p1 wt" type="submit" name="delete" value="true">Sil</button>
+        </div>
+        <br><br><br><br>
+        <h3>Bu oyuna <?php echo $_POST["comment_num"];?> yorum yapılmış. Hepsi kaybolacak .</h3>
+        <br><br><br><br>
+        <h3>Sonsuza kadar.😱</h3>
+        <input type="hidden" name="game_id" value="<?php echo $game_id ?>">
+    </form>
+</div>
 </main>
 <?php include_once "site/footer.php"; ?>
