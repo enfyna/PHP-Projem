6fd55ad874777815db93cca2398e4d277636c4cb
Ramazan Aslan <91965312+enfyna@users.noreply.github.com>
2023-05-20 16:02:25 +0300

    Update footer.php

--- site/footer.php
+++ site/footer.php
@@ -1,14 +1,14 @@
         <footer>
-            <hr class="line hw white"  style="border-color: wheat;">
-            <div style="display:flex;flex-direction: row;align-items:space-around;justify-content: center;">
+            <hr class="line hw white">
+            <div style="horizontal">
                 <span class="dot"></span><span class="dot"></span><span class="dot"></span><span class="dot"></span><span class="dot"></span><span class="dot"></span><span class="dot"></span>
             </div>
-            <div style="display:flex;flex-direction: row;align-items:space-around;justify-content: center;">
-                <a href="https://github.com/enfyna/PHP-Projem"><img src="images/github.svg" height="100%" style="display:block;margin:auto;"></a>
+            <div >
+                <a href="https://github.com/enfyna/enfyna.github.io"><img src="images/github.svg" height="100%" style="display:block;margin:auto;"></a>
             </div>
             <div style="color: darkgray;">&copy;</div>
             <div style="color: darkgray;">2023-2023</div>
-            <hr class="line white" style="border-color: wheat;">
+            <hr class="line white">
         </footer>
     </body>
 </html>
