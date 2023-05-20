c43737366f7f5e165512ac065aa9f4be13656076
Ramazan Aslan <91965312+enfyna@users.noreply.github.com>
2023-05-20 16:21:58 +0300

    Update kick_user.php

--- library/kick_user.php
+++ library/kick_user.php
@@ -1,8 +1,8 @@
 <?php
 require_once "library/functions.php";
 require_once "library/config.php";
-//echo json_encode($_SESSION);
 if (isset($_SESSION['user_id'])) {
+  // Giriş yapan kişileri ana sayfaya at
   redirectTo("index.php",0);
 };
 ?>
