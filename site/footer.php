d843b2029d960262356f8d0080e0127fbb10ea7c
Ramazan Aslan <91965312+enfyna@users.noreply.github.com>
2023-05-20 15:57:01 +0300

    Update footer.php

--- site/footer.php
+++ site/footer.php
@@ -4,7 +4,7 @@
                 <span class="dot"></span><span class="dot"></span><span class="dot"></span><span class="dot"></span><span class="dot"></span><span class="dot"></span><span class="dot"></span>
             </div>
             <div style="display:flex;flex-direction: row;align-items:space-around;justify-content: center;">
-                <a href="https://github.com/enfyna/enfyna.github.io"><img src="images/github.svg" height="100%" style="display:block;margin:auto;"></a>
+                <a href="https://github.com/enfyna/PHP-Projem"><img src="images/github.svg" height="100%" style="display:block;margin:auto;"></a>
                 <!-- <div style="margin:30px">Disclaimer</div><div style="margin:30px">Privacy Policy</div><div style="margin:30px">Terms of Use</div><div style="margin:30px">Contact Us</div> -->
             </div>
             <div style="color: darkgray;">&copy;</div>
