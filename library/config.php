ed9efb4c401a153d12c38aeaf5a653e50671aa0f
Ramazan Aslan <91965312+enfyna@users.noreply.github.com>
2023-05-20 16:09:47 +0300

    Update config.php

--- library/config.php
+++ library/config.php
@@ -1,5 +1,4 @@
 <?php
-
 error_reporting(E_ALL); 
 ini_set('display_errors', 1); // Hata Mesajlarını Yazdır
 
@@ -7,17 +6,11 @@ $db_host = "localhost";
 $db_user = "root";
 $db_pass = "";
 $db_name = "db1";
-
 $db = mysqli_connect($db_host, $db_user, $db_pass, $db_name);
 
-if(mysqli_connect_errno())
-{
+if(mysqli_connect_errno()){
     echo "Bağlantı kurulamadı!";
     exit;
-}
-
+};
 mysqli_set_charset($db,"utf8mb4");
-
 session_start();
-
-///echo "Bağlantı başarılı...";
